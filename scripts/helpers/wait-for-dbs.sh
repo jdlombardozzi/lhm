6422d9284ea3f88eca7be42d0b506645373dcaeb
Etienne Berube <84141531+EtienneBerubeShopify@users.noreply.github.com>
2021-10-06 12:09:50 -0400

    Changed dev.yml to use wait script

---
+++ scripts/helpers/wait-for-dbs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Wait for writer
+echo "Waiting for MySQL-1: "
+while ! (mysqladmin ping --host="127.0.0.1" --port=33006 --user=root --password=password --silent 2> /dev/null); do
+  echo -ne "."
+  sleep 1
+done
+# Wait for reader
+echo "Waiting for MySQL-2: "
+while ! (mysqladmin ping --host="127.0.0.1" --port=33007 --user=root --password=password --silent 2> /dev/null); do
+  echo -ne "."
+  sleep 1
+done
+# Wait for proxysql
+echo "Waiting for ProxySQL:"
+while ! (mysqladmin ping --host="127.0.0.1" --port=33005 --user=root --password=password --silent 2> /dev/null); do
+  echo -ne "."
+  sleep 1
+done
+
+echo "All DBs are ready"
